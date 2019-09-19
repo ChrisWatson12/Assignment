d74acdea8d2e23fa98d59cf99c7db0b99719624b
Shivam <raajpoot90@gmail.com>
2019-09-19 23:05:27 +0530

    code modification

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,16 +3,12 @@ import {Dimensions, PixelRatio} from 'react-native';
 const {height: screenHeight, width: screenWidth} = Dimensions.get('screen');
 
 const widthPercentageToDP = widthPercent => {
-  const elemWidth =
-    typeof widthPercent === 'number' ? widthPercent : parseFloat(widthPercent);
+  const elemWidth = typeof widthPercent === 'number' ? widthPercent : parseFloat(widthPercent);
   return PixelRatio.roundToNearestPixel((screenWidth * elemWidth) / 100);
 };
 
 const heightPercentageToDP = heightPercent => {
-  const elemHeight =
-    typeof heightPercent === 'number'
-      ? heightPercent
-      : parseFloat(heightPercent);
+  const elemHeight = typeof heightPercent === 'number' ? heightPercent : parseFloat(heightPercent);
   return PixelRatio.roundToNearestPixel((screenHeight * elemHeight) / 100);
 };
 
